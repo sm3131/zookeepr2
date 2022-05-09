2ded4b66fc1dc18d6d0e6bcb1efb8371378b7b57
sm3131 <sm2683@nau.edu>
2022-05-09 15:55:09 -0700

    created POST routes and functions/validations to create a new animal

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
+const fs = require('fs');
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -11,15 +34,15 @@ function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
 
     if (query.personalityTraits) {
-        
+
         if (typeof query.personalityTraits === 'string') {
             personalityTraitsArray = [query.personalityTraits];
         } else {
             personalityTraitsArray = query.personalityTraits;
         }
-        
+
         personalityTraitsArray.forEach(trait => {
-            
+
             filteredResults = filteredResults.filter(
                 animal => animal.personalityTraits.indexOf(trait) !== -1
             );
@@ -38,10 +61,22 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
-function findById (id, animalsArray) {
+function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
 
     return result;
+}
+
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    return animal;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -58,12 +93,23 @@ app.get('/api/animals', (req, res) => {
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
 
-    if(result) {
+    if (result) {
         res.json(result);
     } else {
         res.send(404);
     }
-    
+
+})
+
+app.post('/api/animals', (req, res) => {
+    req.body.id = animals.length.toString();
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
 })
 
 app.listen(PORT, () => {
