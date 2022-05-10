94d0cc9daf03929016684ec8e77990ff37560376
sm3131 <sm2683@nau.edu>
2022-05-09 17:05:59 -0700

    added front-end files and connected client to server to serve up html, static files, and json data

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function validateAnimal(animal) {
     if (!animal.name || typeof animal.name !== 'string') {
         return false;
@@ -111,6 +113,22 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'))
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
